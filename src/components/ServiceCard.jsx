02a1e7cb20e1f6984b8a43f42aa45e4d9c75cca8
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2024-05-15 15:07:46 +0300

    feat: Add slider to small layout and finalize section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -6,7 +6,7 @@ export const ServiceCard = ({
 }) => {
   return (
     <div
-      className={`bg-${background} rounded-[8px] px-9 py-20 text-center tracking-wider`}
+      className={`bg-${background} rounded-[8px] px-12 wide:px-9 py-14 wide:py-20 w-[288px] h-[288px] wide:w-full wide:h-full text-center tracking-wider`}
     >
       <div className='flex justify-center mb-3'>
         <img
@@ -17,9 +17,7 @@ export const ServiceCard = ({
       <h3 className='mb-3 text-2xl3 text-primary-navy tracking-widest'>
         {label}
       </h3>
-      <p>
-        {subtext}
-      </p>
+      <p>{subtext}</p>
     </div>
   );
 };
--- src/sections/Comfort.jsx
+++ src/sections/Comfort.jsx
@@ -1,3 +1,8 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css';
+
 import { Button, ServiceCard } from '../components';
 import stars from '../assets/images/stars.png';
 import { services } from '../constants';
@@ -17,6 +22,30 @@ export const Comfort = () => {
           />
         ))}
       </div>
+
+      <Swiper
+        slidesPerView={'auto'}
+        loop
+        modules={[Navigation, Autoplay]}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: true,
+          pauseOnMouseEnter: true,
+        }}
+        navigation
+        grabCursor
+        spaceBetween={10}
+        className='wide:hidden max-w-[288px]'
+      >
+        {services.map((service, index) => (
+          <SwiperSlide key={index}>
+            <ServiceCard
+              key={service.label}
+              {...service}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div className='flex flex-col items-center justify-center mt-10 wide:mt-14'>
         <Button />
